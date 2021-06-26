7c347f1f5d5ccf44c9fb2b99ebebf5a413cf43a1
Samin1615088 <samin.shafkat@gmail.com>
2021-06-26 19:19:32 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const mongoose = require('mongoose');
+require('dotenv').config()
+const authRoute = require('./routes/auth')
+const postsRoute = require('./routes/posts')
+var cors = require('cors');
+
+var app = express();
+
+// Then use it before your routes are set up:
+app.use(cors());
+
+const port = process.env.PORT || 5000
+
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URL,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    })
+    .then(console.log("Connected to MongoDB"))
+    .catch((err) => console.log(err));
+
+app.use('/api/auth', authRoute)
+app.use('/api/posts', postsRoute)
+
+// app.post('/api/auth', async (req, res) => {
+//     try {
+//         const { body } = req
+//     } catch (err) {
+//         console.log(err)
+//         res.send(err.message)
+//     }
+// })
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
